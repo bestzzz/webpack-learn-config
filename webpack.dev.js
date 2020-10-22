11ada400d55255289da17f894aa3234925c4924d
左钊 <zuozhao@mmears.com>
2020-10-22 19:47:38 +0800

    区分不同环境

---
+++ webpack.dev.js
@@ -0,0 +1,8 @@
+const {merge} = require('webpack-merge');
+const baseConfig = require('./webpack.config');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  // devServer: {},
+  // devtool: 'source-map'
+});
